88685dbd2988b361be769521e7c998fdf6c69aa0
PRDGY <iprokhorov08@gmail.com>
2025-03-01 13:17:19 +0300

    Добавка

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
+import sqlite3
+from PyQt6 import uic
+from PyQt6.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
 
+
+class CoffeeApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
+        self.load_data()
+
+    def load_data(self):
+        conn = sqlite3.connect('coffee.sqlite')
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM coffee")
+        alls = cursor.fetchall()
+        self.tableWidget.setRowCount(len(alls))
+        self.tableWidget.setColumnCount(7)
+        for i, row in enumerate(alls):
+            for j, item in enumerate(row):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(item)))
+        conn.close()
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+    window = CoffeeApp()
+    window.show()
+    app.exec()
